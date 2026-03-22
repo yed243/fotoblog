67110c0a22f39fdf6a511b155dfef2b871b6824b
yed243 <oliviermukiese243@gmail.com>
2026-03-22 22:53:43 +0100

    Changes to be committed: 	new file:   authentication/__pycache__/__init__.cpython-313.pyc 	new file:   authentication/__pycache__/admin.cpython-313.pyc 	new file:   authentication/__pycache__/apps.cpython-313.pyc 	new file:   authentication/__pycache__/models.cpython-313.pyc 	new file:   authentication/migrations/0001_initial.py 	new file:   authentication/migrations/__pycache__/0001_initial.cpython-313.pyc 	new file:   authentication/migrations/__pycache__/__init__.cpython-313.pyc 	modified:   authentication/models.py 	new file:   blog/__pycache__/__init__.cpython-313.pyc 	new file:   blog/__pycache__/admin.cpython-313.pyc 	new file:   blog/__pycache__/apps.cpython-313.pyc 	new file:   blog/__pycache__/models.cpython-313.pyc 	new file:   blog/migrations/__pycache__/__init__.cpython-313.pyc 	new file:   db.sqlite3 	modified:   fotoblog/__pycache__/settings.cpython-313.pyc 	new file:   fotoblog/__pycache__/urls.cpython-313.pyc 	modified:   fotoblog/settings.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES = (
+        (CREATOR,'Createur'),
+        (SUBSCRIBER, 'Abonné')
+    
+    )
+
+    profile_photo = models.ImageField(verbose_name='photo de profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='role')
